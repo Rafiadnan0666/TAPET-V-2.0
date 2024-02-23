0a111d866bf4b6f87ac84feb27e226305fddc777
Rafiadnan06666 <fn2344561@gmail.com>
2024-02-23 08:11:47 +0700

    d

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+             $table->enum("role", ["l", "u"]);
             $table->string("gambar");
             $table->timestamps();
         });
