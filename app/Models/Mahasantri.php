72dc001ca8a420fc6fa96e6b1ab3ec42693e86c9
ah-badrr <ahmadibadurrahman03@gmail.com>
2024-02-23 07:47:33 +0700

    Menambahkan relasi di models

--- app/Models/Mahasantri.php
+++ app/Models/Mahasantri.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Setoran;
+use App\Models\User;
+
 
 class Mahasantri extends Model
 {
     use HasFactory;
+    protected $table = "mahasantri";
+
+    public function mentor() {
+        return $this->belongsTo(User::class,'mentor_id','id');
+    }
+
+    public function setoran() {
+        return $this->hasMany(Setoran::class,'mahasantri_id','id');
+    }
 }
--- app/Models/Setoran.php
+++ app/Models/Setoran.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Mahasantri;
 
 class Setoran extends Model
 {
     use HasFactory;
+    protected $table = "setoran";
+
+    public function mahasantri() {
+        return $this->belongsTo(Mahasantri::class,'mahasantri_id','id');
+    }
 }
