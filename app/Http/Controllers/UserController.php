604f3013e34bc40951388aa43c6a682b121d44c0
alghozy <ibadalghozy11@gmail.com>
2024-03-05 20:50:26 +0700

    Menmabahkan crud untuk user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,9 +13,18 @@ class UserController extends Controller
     /**
      * Display a listing of the resource.
      */
+
+     public $mentor;
+
+     public function __construct()
+     {
+         $this->mentor = new User();
+     }
     public function index()
     {
-        //
+        $mentor=Mentor::where('role', '=', 'm')->Paginate(5);
+        $no = 5 * ($mentor->currentPage() - 1);
+        return view("user.index",compact('mentor', 'no'));
     }
 
     /**
@@ -23,7 +32,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        return view('user.create');
     }
 
     /**
@@ -31,7 +40,32 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $rules = [
+            //format penulisan untuk field yang unik = unique:nama_tabel,field_tabel
+            'nama_kategori' => 'required|min:3|max:20|unique:kategori,kategori',
+            // 'jenis' => 'required|max:20|unique'
+        ];
+        //bikin pesan error
+        $messages = [
+            'required' => ':attribute gak boleh kosong masseeh',
+            'min' => ':attribute minimal harus 3 huruf',
+            'max' => ':attribute maximal 20 huruf',
+            'unique' => "attribut sudah ada, gunakan yang lain"
+        ];
+
+        //eksekusi fungsinya
+        $this->validate($request, $rules, $messages);
+        //pasangkan ke field tabelnya dengan kiriman user
+        $this->kategori->kategori = $request->nama_kategori;
+
+        //lalu simpan ke database
+        $this->kategori->save();
+
+        Alert::success('Success Title', 'Success Message');
+
+
+        //redirect
+        return redirect()->route('kategori');
     }
 
     /**
@@ -39,7 +73,11 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $mentor = Kategori::findOrFail($id);
+
+        // buat ngecek data terkirim atau nggak
+        // dd($mentor);
+        return view('kategori.show', compact('kategori'));
     }
 
     /**
@@ -47,7 +85,9 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $mentor = Kategori::findOrFail($id);
+
+        return view('kategori.edit', compact('kategori'));
     }
 
     /**
@@ -55,7 +95,38 @@ class UserController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $update = Kategori::findOrFail($id);
+        //cel ada perubahan atau nggak
+        //isDirty() buat ngecek field tabel ada perubahan atau nggak
+        //dengan kiriman data dari from
+        $update->kategori = $request->nama_kategori;
+        if ($update->isDirty()) {
+            echo "ada perubahan";
+        } else {
+            echo "tidak ada perubahan";
+        }
+
+        $rules = [
+            'kategori' => 'required|min:3|max:20|unique:kategori,kategori',
+        ];
+
+        $messages = [
+            'required' => ':attribute gak boleh kosong masseeh',
+            'min' => ':attribute minimal harus 3 huruf',
+            'max' => ':attribute maximal 20 huruf',
+            'unique' => "attribut sudah ada, gunakan yang lain"
+        ];
+
+        $this->validate($request, $rules, $messages);
+
+        // Update data berdasarkan ID
+        $mentor = Kategori::findOrFail($id);
+        $mentor->kategori = $request->kategori;
+        $mentor->save();
+
+        Alert::success('Success Title', 'Success Message');
+
+        return redirect()->route('kategori');
     }
 
     /**
@@ -63,6 +134,14 @@ class UserController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         //ngaaaaapus data
+        //ambil data sesuai IDnya
+        $mentor = Kategori::findOrFail($id);
+        //buat fungsi ngapus data
+        $mentor->delete();
+
+        Alert::success('Successpull', 'Data berhasil di hapus');
+        //redirect halaman
+        return redirect()->route('kategori');
     }
 }
