9c5b3d82b055ede4187b3dc9bde41cf6a9987d99
alghozy <ibadalghozy11@gmail.com>
2024-03-05 21:25:52 +0700

    Menmabahkad view untuk user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,9 +11,6 @@ use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
 
     public $mentor;
     public $mahasantri;
@@ -30,25 +27,19 @@ class UserController extends Controller
         return view("user.index", compact('mentor', 'no'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
+
     public function create()
     {
         return view('user.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+
     public function store(Request $request)
     {
         $rules = [
-            //format penulisan untuk field yang unik = unique:nama_tabel,field_tabel
             'nama_mentor' => 'required|min:3|max:20|unique:mentor,mentor',
-            // 'jenis' => 'required|max:20|unique'
         ];
-        //bikin pesan error
+   
         $messages = [
             'required' => ':attribute gak boleh kosong masseeh',
             'min' => ':attribute minimal harus 3 huruf',
@@ -56,36 +47,28 @@ class UserController extends Controller
             'unique' => "attribut sudah ada, gunakan yang lain"
         ];
 
-        //eksekusi fungsinya
+   
         $this->validate($request, $rules, $messages);
-        //pasangkan ke field tabelnya dengan kiriman user
         $this->mentor->mentor = $request->nama_mentor;
 
-        //lalu simpan ke database
+  
         $this->mentor->save();
 
         Alert::success('Success Title', 'Success Message');
 
 
-        //redirect
         return redirect()->route('mentor');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         $mentor = User::findOrFail($id);
 
-        // buat ngecek data terkirim atau nggak
-        // dd($mentor);
         return view('mentor.show', compact('mentor'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
+
     public function edit(string $id)
     {
         $mentor = User::findOrFail($id);
@@ -93,15 +76,11 @@ class UserController extends Controller
         return view('mentor.edit', compact('mentor'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+
     public function update(Request $request, string $id)
     {
         $update = User::findOrFail($id);
-        //cel ada perubahan atau nggak
-        //isDirty() buat ngecek field tabel ada perubahan atau nggak
-        //dengan kiriman data dari from
+    
         $update->mentor = $request->nama_mentor;
         if ($update->isDirty()) {
             echo "ada perubahan";
@@ -122,7 +101,6 @@ class UserController extends Controller
 
         $this->validate($request, $rules, $messages);
 
-        // Update data berdasarkan ID
         $mentor = User::findOrFail($id);
         $mentor->mentor = $request->mentor;
         $mentor->save();
@@ -132,19 +110,15 @@ class UserController extends Controller
         return redirect()->route('mentor');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+
     public function destroy(string $id)
     {
-        //ngaaaaapus data
-        //ambil data sesuai IDnya
+
         $mentor = User::findOrFail($id);
-        //buat fungsi ngapus data
         $mentor->delete();
 
         Alert::success('Successpull', 'Data berhasil di hapus');
-        //redirect halaman
+     
         return redirect()->route('mentor');
     }
 }
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,78 @@
+@extends('master.dash')
+@section('konten')
+    <div class="row mt-5">
+        <div class="col-md-12">
+            <div class="white_shd full margin_bottom_30">
+                <div class="full graph_head">
+                    <div class=" d-flex justify-content-between">
+                        <h2>Data Setoran</h2>
+                        <a href="{{ route('setoran.create') }}">
+                            <button class="btn btn-primary"><i class="fa fa-create"></i>tambah </button>
+                        </a>
+                    </div>
+                </div>
+                <div class="table_section padding_infor_info">
+                    <div class="table-responsive-sm">
+                        <table class="table table-hover">
+                            <thead class="thead-dark">
+                                <tr>
+                                    <th>#</th>
+                                    <th>Mahasantri</th>
+                                    <th>Tanggal</th>
+                                    <th>Juz</th>
+                                    <th>Halaman</th>
+                                    <th>Nilai</th>
+                                    <th>Status</th>
+                                    <th>Keterangan</th>
+                                    <th>Created At</th>
+                                    <th>Updated At</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($setoran as $mhs)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $mhs->mahasantri->nama_mhs }}</td>
+                                        <td>{{$mhs->tanggal}}</td>
+                                        <td>{{ $mhs->juz }}</td>
+                                        <td>{{ $mhs->halaman }}</td>
+                                        <td>{{ $mhs->nilai }}</td>
+                                        <td>{{ $mhs->status == 'l' ? 'lanjut' : 'ulang' }}</td>
+                                        <td>
+                                            @if (Str::length($mhs->keterangan) > 10)
+                                                {{ substr($mhs->keterangan, 0, 10) . "[.....]" }}
+                                            @else
+                                            {{$mhs->keterangan}}
+                                            @endif
+                                        </td>                                        
+                                        <td>{{ $mhs->created_at }}</td>
+                                        <td>{{ $mhs->updated_at }}</td>
+                                        <td class="d-flex">
+                                            <a href="{{ route('setoran.show', $mhs->id) }}">
+                                                <button class="btn btn-primary"><i class="fa fa-eye"></i></button>
+                                            </a>
+                                            <a href="{{ route('setoran.edit', $mhs->id) }}">
+                                                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                            </a>
+
+                                            <form action="{{ route('setoran.destroy', $mhs->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger"><i
+                                                        class="fa fa-trash"></i></button>
+                                            </form>
+
+                                        </td>
+                                    </tr>
+                                @endforeach
+
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
