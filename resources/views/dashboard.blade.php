c690517fc99b54454fd3535ce156e98bb667eb52
ah-badrr <ahmadibadurrahman03@gmail.com>
2024-03-05 21:36:50 +0700

    Edit dashboard dikit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -26,7 +26,7 @@
     </div>
     <div class="row column1">
         <div class="col-md-6 col-lg-3">
-            <a href="{{ route('mentor.index') }}">
+            <a href="{{ route('user.index') }}">
                 <div class="full counter_section margin_bottom_30">
                     <div class="couter_icon">
                         <div>
@@ -37,8 +37,6 @@
                         <div>
 
                             <p class="total_no">{{ $user->count() }}</p>
-
-
 
                             <p class="head_couter">Mentor</p>
                         </div>
